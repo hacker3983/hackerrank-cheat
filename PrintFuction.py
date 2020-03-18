31a86759e4f047a9080dda8cecbf66d02856aaf2
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-03-18 14:37:28 -0400

    Create PrintFuction.py

---
+++ PrintFuction.py
@@ -0,0 +1,5 @@
+print(*range(1, int(input())+1), sep='') # do the same as down below
+or
+# n = int(input()) will make you input any number then count to the number
+# for i in range(1,n+1): it will add 1 every time to reach number you entered
+       # print(i,end="") will start from 1 to number you entered ends
